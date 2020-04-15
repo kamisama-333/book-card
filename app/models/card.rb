b1d5b1006548ccea89e295d24fbf07dfa12582ad
kamisama-333 <akaiposuto5160@icloud.com>
2020-04-15 16:27:35 +0900

    function

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,3 @@
 class Card < ApplicationRecord
+  validates :title, :author, :price, :publisher, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   root to: 'cards#index'
-  resources :cards, only: [:new, :create]
-  get 'cards/show'
-  get 'cards/add'
-  get 'cards/edit'
+  resources :cards, except: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
