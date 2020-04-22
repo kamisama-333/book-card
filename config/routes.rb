234e24bfae15d0d408982fe31f440e3fc971f95d
kamisama-333 <akaiposuto5160@icloud.com>
2020-04-22 17:19:45 +0900

    search

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'cards#index'
-  resources :cards, except: :index
+  resources :cards, except: :index do
+    collection do
+      get 'search'
+    end
+  end
   resources :users, only: :show
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
