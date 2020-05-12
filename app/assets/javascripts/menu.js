af3bf92ecb9b3fbfee386da511a27a836eb0d5f3
kamisama-333 <akaiposuto5160@icloud.com>
2020-05-12 20:47:12 +0900

    menu-js

---
+++ app/assets/javascripts/menu.js
@@ -0,0 +1,14 @@
+$(function () {
+  // クリック時の動作
+  $('.hamberger_line').on('click', function() {
+      // メニューを閉じる
+      if($(this).hasClass('open')) {
+          $(this).removeClass('open');
+          $('.hamberger_list').removeClass('open');
+      // メニューを開く
+      } else {
+          $(this).addClass('open');
+          $('.hamberger_list').addClass('open');
+      }
+  });
+});
