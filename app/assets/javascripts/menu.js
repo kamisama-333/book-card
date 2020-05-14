4d53a49b63b7a327df920b2937416c342fdc5251
kamisama-333 <akaiposuto5160@icloud.com>
2020-05-14 21:44:54 +0900

    Update menu.js

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,17 +1,17 @@
 $(function () {
   // クリック時の動作
   $('.hamberger_line').on('click', function() {
-      // メニューを閉じる
-      if($(this).hasClass('open')) {
-          $(this).removeClass('open');
-          $('.hamberger_list').removeClass('open');
-          $('.overlay').removeClass('open');
-      // メニューを開く
-      } else {
-          $(this).addClass('open');
-          $('.hamberger_list').addClass('open');
-          $('.overlay').addClass('open');
-        }
+    // メニューを閉じる
+    if($(this).hasClass('open')) {
+        $(this).removeClass('open');
+        $('.hamberger_list').removeClass('open');
+        $('.overlay').removeClass('open');
+        // メニューを開く
+    } else {
+        $(this).addClass('open');
+        $('.hamberger_list').addClass('open');
+        $('.overlay').addClass('open');
+    }
   });
   $('.overlay').on('click',function(){
     if($(this).hasClass('open')){
