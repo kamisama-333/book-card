d3e2acebf0c6618987719df5b174eca3043e834b
kamisama-333 <akaiposuto5160@icloud.com>
2020-05-14 21:35:42 +0900

    overlay-js & scss

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -5,10 +5,21 @@ $(function () {
       if($(this).hasClass('open')) {
           $(this).removeClass('open');
           $('.hamberger_list').removeClass('open');
+          $('.overlay').removeClass('open');
       // メニューを開く
       } else {
           $(this).addClass('open');
           $('.hamberger_list').addClass('open');
-      }
+          $('.overlay').addClass('open');
+        }
+  });
+  $('.overlay').on('click',function(){
+    if($(this).hasClass('open')){
+        $(this).removeClass('open');
+        $('.hamberger_line').removeClass('open');
+        $('.hamberger_list').removeClass('open');
+      
+    }
   });
 });
+
